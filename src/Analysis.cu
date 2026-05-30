88da072556bbf38e454d0cdb0c1c92e67d00e1e1
Benhou Xiang <aurelianox@qq.com>
2026-05-30 16:04:15 +0800

    some debug

--- src/Analysis.cu
+++ src/Analysis.cu
@@ -3720,39 +3720,3 @@ private:
     }
 
 };
-
-// 定义Python模块
-PYBIND11_MODULE(ctpwa, m)
-{
-    m.doc() = "ctpwa";
-
-    pybind11::class_<analysis>(m, "analysis")
-        .def(pybind11::init<const std::string&>(), pybind11::arg("config_file") = "config.yml")
-        .def("getNLL", &analysis::getNLL, pybind11::arg("params"),
-             "Compute NLL. params: [real(v), imag(v), theta] float64")
-        .def("getNVector", &analysis::getNVector)
-        .def("getNFreeTheta", &analysis::getNFreeTheta)
-        .def("getNParams", &analysis::getNParams)
-        .def("getSLVectors", &analysis::getSLVectors)
-        .def("writeResult", &analysis::writeResult)
-        .def("getHessian", &analysis::getHessian, pybind11::arg("params"),
-             "Full Hessian (2n+P)×(2n+P). params: [real(v), imag(v), theta] float64")
-        .def("getDataTensor", &analysis::getDataTensor)
-        .def("getPhspTensor", &analysis::getPhspTensor)
-        // .def("getTruthTensor", &analysis::getTruthTensor)
-        .def("testBWRHessian", &analysis::testBWRHessian,
-             pybind11::arg("m"), pybind11::arg("m0"), pybind11::arg("g0"),
-             pybind11::arg("L"), pybind11::arg("q"), pybind11::arg("q0"), pybind11::arg("d") = 3.0,
-             "Test AutoDiff BWR Hessian. Returns [R_re,R_im, dRe_dm0,dRe_dg0,dIm_dm0,dIm_dg0, d2Re_dm02,d2Re_dm0dg0,d2Re_dg02, d2Im_dm02,d2Im_dm0dg0,d2Im_dg02]")
-        .def("getBranchFractions", &analysis::getBranchFractions)
-        .def("getBkgTensor", &analysis::getBkgTensor)
-        .def("getBkgWeightsTensor", &analysis::getBkgWeightsTensor)
-        .def("saveSLAmps", &analysis::saveSLAmps)
-        .def("getConstraintsIndex", &analysis::getConstraintsIndex)
-        .def("getConstraintsValues", &analysis::getConstraintsValues)
-        .def("getAmplitudeNames", &analysis::getAmplitudeNames)
-        .def("getNPolarizations", &analysis::getNPolarizations)
-        .def("reCalcAmp", &analysis::reCalcAmp)
-        .def("getFreeResParams", &analysis::getFreeResParams)
-        .def("isValid", &analysis::isValid);
-}
