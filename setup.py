3e2499f2e1263ebef0269b9673ad201dbdfd62b8
Benhou Xiang <aurelianox@qq.com>
2026-09-18 00:40:05 +0800

    chore: 发布 v0.3.8

--- setup.py
+++ setup.py
@@ -223,7 +223,7 @@ except Exception:
 
 setup(
     name="ctpwa",
-    version="0.3.7",
+    version="0.3.8",
     author="Benhou Xiang",
     description="CUDA-Torch Partial Wave Analysis",
     long_description=_long_desc,
